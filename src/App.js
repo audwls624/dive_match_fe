b7f8b5bb53cde4dabfcf7a9bb14592ae90951037
jin <audwls624@gmail.com>
2023-12-06 01:18:37 +0900

    App.js 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import api from './api'
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Hello! Welcome to Dive Match
-      </h1>
-    </div>
-  );
+const App = () => {
+  const [certificates, setCertificates] = useState([]);
+  const [formData, setFormData] = useState({
+    id: '',
+    name: ''
+  });
+
+  const fetchCertificates = async () => {
+    const response = await api.get('/api/user/certificates/');
+    setCertificates(response.data);
+  }
+
+  useEffect(() => {
+    fetchCertificates();
+  }, []);
+
+  const handleInputChange = (event) => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    setFormData({
+      ...formData,
+      [event.target.name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    // await api.post()
+  }
 }
 
 export default App;
